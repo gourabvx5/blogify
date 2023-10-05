9d44a9e6b9faef2f8d6547386665687dce63dde3
TusharImran-Dev <tusharimran.dev@gmail.com>
2023-10-05 11:50:55 +0600

    Some Component & Page Template Added

---
+++ resources/views/components/page/hero.blade.php
@@ -0,0 +1,3 @@
+<div style="height:100vh;display:flex;align-items:center;justify-content:center;">
+    <h1 style="font-size:46px; text-align:center;">Blogify<span style="font-size:20px;font-weight:400;"> - Create a unique and beautiful blog easily</span></h1>
+</div>
---
+++ resources/views/pages/home.blade.php
@@ -0,0 +1,11 @@
+@extends('layout.app')
+
+@section('title')
+    Create a unique and beautiful blog easily
+@endsection
+
+@section('content')
+    <x-page.hero/>
+    <x-page.posts/>
+    <x-page.footer/>
+@endsection
