9dc82c1bc31efdb34a878cf298088d9ccb86ba39
gourabvx5 <gourabvx55@gmail.com>
2023-10-08 16:48:01 +0600

    User Create & Login Controller Mock Added

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,22 +2,50 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    public function userLogin()
-    {
-        return view('pages.auth.login');
-    }
-
-    public function userCreate()
-    {
-        return "OK";
-    }
 
     public function userRegister()
     {
         return view('pages.auth.registration');
     }
+    public function userCreate(Request $request)
+    {
+        $userdata = $request->validate([
+            'name'=> 'required',
+            'email'=> 'required',
+            'password'=>'required',
+        ]);
+
+        User::create($userdata);
+
+
+        return redirect('pages.auth.login');
+
+    }
+
+    public function user_authenticate(Request $request) {
+        $formFields = $request->validate([
+            'email' => ['required', 'email'],
+            'password' => 'required'
+        ]);
+
+        if(auth()->attempt($formFields)) {
+            $request->session()->regenerate();
+
+            return redirect('/');
+        }
+
+        // return back()->withErrors(['email' => 'Invalid Credentials'])->onlyInput('email');
+    }
+
+    public function userLogin()
+    {
+        return view('pages.auth.login');
+    }
+
+
 }
